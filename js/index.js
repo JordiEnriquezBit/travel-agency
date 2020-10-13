be37f452080a718ccd9b8e7a933699c8910aaf7e
Jordi <jordi.enriquez@student.bit.es>
2020-10-13 12:50:49 +0200

    sum all prices in form

---
+++ js/index.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+    let form = document.getElementById("form");
+    form.addEventListener("submit", logSubmit);
+
+}
+
+function logSubmit(event) {
+    event.preventDefault();
+}
+
+
+function changeHoteles() {
+    let value = document.getElementById("numberHotel").value;
+    if (value >= 1 && value <= 50) {
+        document.getElementById("priceHotel").innerHTML = value * 60;
+    }
+    else {
+        document.getElementById("numberHotel").value = 1;
+        document.getElementById("priceHotel").innerHTML = 60;
+
+    }
+    changeTotalPrice()
+}
+
+function changeVuelos() {
+    let value = document.getElementById("vueloSelect").value;
+    document.getElementById("priceVuelo").innerHTML = value;
+    changeTotalPrice()
+}
+
+
+function changeCoche() {
+    let value = document.getElementById("numberCoche").value;
+    let price = 0;
+    if (value >= 1 && value <= 50) {
+        if (value >= 7) {
+            price = value * 40 - 50
+
+        } else if (value >= 3) {
+            price = value * 40 - 20;
+
+        } else {
+            price = value * 40;
+        }
+        document.getElementById("priceCoche").innerHTML = price;
+
+    }
+    else {
+        document.getElementById("numberCoche").value = 1;
+        document.getElementById("priceCoche").innerHTML = 40;
+
+    }
+    changeTotalPrice();
+
+}
+
+
+function changeTotalPrice() {
+    let hotel = Number.parseInt(document.getElementById("priceHotel").innerHTML);
+    let vuelo = Number.parseInt(document.getElementById("priceVuelo").innerHTML);
+    let coche = Number.parseInt(document.getElementById("priceCoche").innerHTML);
+
+    console.log("h: "+hotel+ " v:"+vuelo+" c: "+coche);
+    document.getElementById("totalPrice").innerHTML = hotel + vuelo + coche;
+
+
+}
